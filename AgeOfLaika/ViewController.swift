ec663e939d9717f90daeffab21b4d7b7382ca5a6
Meng Fung <meng.fung@gmail.com>
2014-09-23 17:25:07 -0700

    designed storyboard and added code to convert human years into dog years

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var humanYearsTextField: UITextField!
+  @IBOutlet weak var dogYearsLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func convertToDogYearsButtonPress(sender: UIButton) {
+    let intHumanYears = humanYearsTextField.text.toInt()!
+    let conversionDogYears = 7
+    
+    let convertedYears = intHumanYears * conversionDogYears
+    dogYearsLabel.hidden = false
+    dogYearsLabel.text = "\(convertedYears) in Dog Years"
+  }
+  
 
 }
 
